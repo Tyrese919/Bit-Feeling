0a2181ccf6348cc5545002c594098047672a28eb
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2021-06-01 12:16:37 +0200

    Feelings Page done
    Done with the Feelings page, next thing is making the result version.

--- App/app/src/main/java/com/example/android/bitfeeling/FeelingsActivity.java
+++ App/app/src/main/java/com/example/android/bitfeeling/FeelingsActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.bitfeeling;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,33 @@ public class FeelingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feelings_activity);
+
+        ImageView happyfeeling = (ImageView) findViewById(R.id.happyfeeling);
+        ImageView neutralfeeling = (ImageView) findViewById(R.id.neutralfeeling);
+        ImageView sadfeeling = (ImageView) findViewById(R.id.sadfeeling);
+
+        happyfeeling.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FeelingsActivity.this, ResultAcitivity.class);
+                startActivity(intent);
+            }
+        });
+
+        neutralfeeling.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FeelingsActivity.this, ResultAcitivity.class);
+                startActivity(intent);
+            }
+        });
+
+        sadfeeling.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FeelingsActivity.this, ResultAcitivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ App/app/src/main/java/com/example/android/bitfeeling/ResultAcitivity.java
@@ -0,0 +1,13 @@
+package com.example.android.bitfeeling;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultAcitivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result_activity);
+    }
+}
