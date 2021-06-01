e85fb4d0a228cf8e23e436b6110a33c86607c127
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2021-06-01 14:09:53 +0200

    Custom Icon, logo implemented
    Made the Icon our Custom logo and implemented the logo into the header of the Login and Feeling screen.

--- App/app/src/main/java/com/example/android/bitfeeling/FeelingsActivity.java
+++ App/app/src/main/java/com/example/android/bitfeeling/FeelingsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,28 +17,33 @@ public class FeelingsActivity extends AppCompatActivity {
         ImageView happyfeeling = (ImageView) findViewById(R.id.happyfeeling);
         ImageView neutralfeeling = (ImageView) findViewById(R.id.neutralfeeling);
         ImageView sadfeeling = (ImageView) findViewById(R.id.sadfeeling);
+        ImageView chat = (ImageView) findViewById(R.id.chat);
 
         happyfeeling.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FeelingsActivity.this, ResultAcitivity.class);
-                startActivity(intent);
+                Toast.makeText(FeelingsActivity.this, getResources().getString(R.string.chatbutton_pressed), Toast.LENGTH_LONG).show();
             }
         });
 
         neutralfeeling.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FeelingsActivity.this, ResultAcitivity.class);
-                startActivity(intent);
+                Toast.makeText(FeelingsActivity.this, getResources().getString(R.string.chatbutton_pressed), Toast.LENGTH_LONG).show();
             }
         });
 
         sadfeeling.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FeelingsActivity.this, ResultAcitivity.class);
-                startActivity(intent);
+                Toast.makeText(FeelingsActivity.this, getResources().getString(R.string.chatbutton_pressed), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FeelingsActivity.this, getResources().getString(R.string.chatbutton_pressed), Toast.LENGTH_LONG).show();
             }
         });
     }
