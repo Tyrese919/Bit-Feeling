5cf57bf5fdda0386bcd31f46613343a4b1815fa7
QuintenSchaap <54798614+QuintenSchaap@users.noreply.github.com>
2021-06-22 15:50:46 +0200

    Classes Done

--- bit mood/classes.php
+++ bit mood/classes.php
@@ -9,33 +9,80 @@
     A {
       text-decoration: none;
     }
+     table, th, td {
+         font-size: 24px;
+         text-align: center;
+         border-collapse: collapse;
+         padding: 5px;
+         width: 400px;
+         border: 1px solid black;
+     }
+
+      table {
+          margin-top: 100px;
+          margin-left: 12%;
+          display: inline-block;
+      }
   </style>
   <body>
-  <?php
-  $servername = "51.68.47.8";
-  $username = "scrumteam5";
-  $password = "G2rc3L400dI024Ce";
-  $dbname = "scrumteam5";
-  $charset = "utf8mb4";
-
-  $dsn = "mysql:host=" . $servername . ";dbname=" . $dbname . ";charset=" . $charset;
-  try {
-      $pdo = new PDO($dsn, $username, $password);
-  } catch (PDOException $e) {
-      print "Error!: " . $e->getMessage() . "<br/>";
-      die();
-  }
-  ?>
-
     <nav>
       <img id="Logo" src="BitMoodtransparantedited.png" />
       <button class="button1"><a href="login.php">Login</a></button>
       <button class="button2"><a href="home.php">Home</a></button>
       <button class="button3"><a href="feelings.php">Feelings</a></button>
     </nav>
+    <?php
 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "bitmood_db";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    $query = "SELECT * FROM student WHERE class_id = 1";
+    $result = mysqli_query($conn, $query);
+
+    echo "<table id='1'>";
+
+    echo "<tr><th>Name</th><th>Class</th></tr><tr>";
+
+    while($row = mysqli_fetch_array($result)){
+        echo "<td>" . $row['name'] . "</td><td>" . $row['class_id'] . "</td></td></tr>";
+    }
+
+    echo "</table>";
+
+    $query2 = "SELECT * FROM student WHERE class_id = 2";
+    $result2 = mysqli_query($conn, $query2);
+
+    echo "<table id='2'>";
+
+    echo "<tr><th>Name</th><th>Class</th></tr><tr>";
+
+    while($row = mysqli_fetch_array($result2)){
+        echo "<td>" . $row['name'] . "</td><td>" . $row['class_id'] . "</td></td></tr>";
+    }
+
+    echo "</table>";
+
+    $query3 = "SELECT * FROM student WHERE class_id = 3";
+    $result3 = mysqli_query($conn, $query3);
+
+    echo "<table id='3'>";
+
+    echo "<tr><th>Name</th><th>Class</th></tr><tr>";
+
+    while($row = mysqli_fetch_array($result3)){
+        echo "<td>" . $row['name'] . "</td><td>" . $row['class_id'] . "</td></td></tr>";
+    }
+
+    echo "</table>";
+
+    mysqli_close($conn);
+    ?>
     <footer>
-      <span> Created By Bit Academy &#169; </span>
+      <span > Created By Bit Academy &#169; </span>
     </footer>
   </body>
 </html>
